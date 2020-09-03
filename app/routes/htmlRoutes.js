0d0708f4a4edd9695d7dba8c10d8a010d42e620e
dennistonc <dennistonc@gmail.com>
2020-09-02 23:00:31 -0500

    added paths to each page, finished styling on home page, will iron out details later

---
+++ app/routes/htmlRoutes.js
@@ -0,0 +1,23 @@
+var path = require("path");
+
+module.exports = function(app) {
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
+    app.get("/about", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/about.html"));
+    });
+
+    app.get("/pricing", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/pricing.html"));
+    });
+
+    app.get("/FAQ", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/FAQ.html"));
+    });
+
+    app.get("/contact", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/contact.html"));
+    });
+};
